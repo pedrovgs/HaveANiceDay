7823d8c8af21104975f08f3313db63ffdcc78189
Pedro Vicente Gómez Sánchez <pedrovgs@users.noreply.github.com>
2017-09-19 08:57:34 +0200

    Finish swagger support integration (#2)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,9 @@
 object Versions {
-
+  val project = "0.1"
+  val scala = "2.12.3"
   val finatra = "2.13.0"
   val logback = "1.2.3"
+  val finatraSwagger = "2.10.0"
   val guice = "4.0"
   val scalatest = "3.0.1"
   val mockito = "1.9.5"
