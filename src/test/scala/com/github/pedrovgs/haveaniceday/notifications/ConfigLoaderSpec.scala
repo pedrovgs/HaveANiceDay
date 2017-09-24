d64edf9a9b7a634e5a6e8364b4a82b2cfda685d6
pedrovgs <pedrovicente.gomez@gmail.com>
2017-09-24 19:15:55 +0200

    Move test resources to a better folder

--- src/test/scala/com/github/pedrovgs/haveaniceday/notifications/ConfigLoaderSpec.scala
+++ src/test/scala/com/github/pedrovgs/haveaniceday/notifications/ConfigLoaderSpec.scala
@@ -7,7 +7,7 @@ import org.scalatest.{FlatSpec, Matchers}
 class ConfigLoaderSpec extends FlatSpec with Matchers {
 
   "ConfigLoader" should "read the config value associated to the api key" in {
-    val loader = new ConfigLoader(ConfigFactory.load("validFirebaseConfig.conf"))
+    val loader = new ConfigLoader(ConfigFactory.load("firebase/validFirebaseConfig.conf"))
 
     val firebaseConfig = loader.loadFirebaseConfig().get
 
@@ -15,7 +15,7 @@ class ConfigLoaderSpec extends FlatSpec with Matchers {
   }
 
   it should "return none if the configuration file is empty" in {
-    val loader = new ConfigLoader(ConfigFactory.load("emptyFirebaseConfig.conf"))
+    val loader = new ConfigLoader(ConfigFactory.load("firebase/emptyFirebaseConfig.conf"))
 
     val firebaseConfig = loader.loadFirebaseConfig()
 
@@ -23,7 +23,7 @@ class ConfigLoaderSpec extends FlatSpec with Matchers {
   }
 
   it should "return none if the configuration file does not contain the expected keys" in {
-    val loader = new ConfigLoader(ConfigFactory.load("invalidFirebaseConfig.conf"))
+    val loader = new ConfigLoader(ConfigFactory.load("firebase/invalidFirebaseConfig.conf"))
 
     val firebaseConfig = loader.loadFirebaseConfig()
 
