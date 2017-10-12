bb482f02831657400dc5592c574c7850a04f964e
pedrovgs <pedrovicente.gomez@gmail.com>
2017-10-12 23:25:44 +0200

    Add smiles table and regenerate tables

---
+++ src/main/resources/db/migration/V1.3__Add_smiles_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE smiles (
+  id                BIGINT(20)    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  creation_date     TIMESTAMP     NOT NULL,
+  photo_url         VARCHAR(2083) NULL,
+  description       VARCHAR(280)  NULL,
+  source            VARCHAR(255)  NOT NULL,
+  sent              BOOL          NOT NULL,
+  sent_date         DATETIME      NULL,
+  sent_smile_number INT           NULL
+);
