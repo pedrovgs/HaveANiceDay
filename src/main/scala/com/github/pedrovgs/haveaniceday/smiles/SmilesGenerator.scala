36340181717202e1f7c5178ef6c8e406d978aaa0
pedrovgs <pedrovicente.gomez@gmail.com>
2017-10-14 00:55:35 +0200

    Fix build error

--- src/main/scala/com/github/pedrovgs/haveaniceday/smiles/SmilesGenerator.scala
+++ src/main/scala/com/github/pedrovgs/haveaniceday/smiles/SmilesGenerator.scala
@@ -71,8 +71,8 @@ class SmilesGenerator @Inject()(config: SmilesGeneratorConfig,
     val lastSmileSentFuture = smilesRepository.getLastSmileSent()
     lastSmileSentFuture.zip(notificationsClient.sendNotificationToEveryUser(notification)).map {
       case (lastSmileSent, Right(_)) =>
-        val smileNumber: Int   = lastSmileSent.flatMap(_.number.map(_ + 1)).getOrElse(1)
-        val smiledMarkedAsSent = smile.copy(sent = true, sentDate = Some(clock.now), number = Some(smileNumber))
+        val smileNumber: Int  = lastSmileSent.flatMap(_.number.map(_ + 1)).getOrElse(1)
+        val smileMarkedAsSent = smile.copy(sent = true, sentDate = Some(clock.now), number = Some(smileNumber))
         Right(smileMarkedAsSent)
       case (_, Left(error)) =>
         Left(UnknownError(
